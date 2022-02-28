17520bf310c391ce961f7c335208fd6ec453297b
Bruno Scoti <bruno.scoti@gmail.com>
2022-02-28 14:11:13 -0300

    font config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,8 +23,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         {/* Use minimum-scale=1 to enable GPU rasterization */}
         <meta name="viewport" content={"user-scalable=0, initial-scale=1," + "minimum-scale=1, width=device-width, height=device-height"} />
-        {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800&display=swap" rel="stylesheet" /> */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800&display=swap" rel="stylesheet" />
         {/* <link rel="shortcut icon" href="https://cdn-portal.centraldofranqueado.com.br/cms/2021/03/26/605dd81f4b704favicon.png" /> */}
         {/* <link rel="preload" href={CDN_POPPINS_FONT_HREF} as="font" type="font/woff2" crossOrigin="anonymous" />
         <link rel="preload" href={CDN_POPPINS_SEMI_BOLD_HREF} as="font" type="font/woff2" crossOrigin="anonymous" />
